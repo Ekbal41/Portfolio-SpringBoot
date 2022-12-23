5e58a56e326b7e408acac0f7ee29aa8c51909523
Ekbal41 <mdasifekbalshagor@gmail.com>
2022-12-23 21:48:58 +0600

    first commit

---
+++ src/main/java/com/asifekbal/portfolio/controller/ViewController.java
@@ -0,0 +1,16 @@
+package com.asifekbal.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+
+
+    @GetMapping("/")
+    public String index(){
+       return "home";
+    }
+    
+}
